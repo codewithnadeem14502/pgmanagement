e34fd6c262c283030deaa13f8f033b5f53d3bfa0
codewithnadeem14502 <codewithnadeem@mgail.com>
2024-01-29 14:46:15 +0000

    deployment

--- server/index.js
+++ server/index.js
@@ -7,11 +7,16 @@ import cors from "cors";
 import isAuth from "./middlewares/isAuth.js";
 dotenv.config();
 import cookieParser from "cookie-parser";
+
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 const app = express();
 
 const PORT = process.env.PORT;
 mongoose
-  .connect(process.env.MONGODB_URL)
+  .connect("mongodb+srv://doadmin:7I8fWg5p906Zq34O@pgdatabase-13de2ab2.mongo.ondigitalocean.com/admin")
   // .connect("mongodb://127.0.0.1:27017/PG")
   .then(() => console.log(`DataBase is connected ${PORT}`));
 
@@ -31,6 +36,11 @@ app.use(
 app.use("/api/v1/pg", pgRouter);
 app.use("/api/v1/tenant", TenantsRouter);
 app.use(isAuth);
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("Server is working");
