aa077cf8cfb2a3d105d006ec4d9b5f9bfcc9d447
codewithnadeem14502 <codewithnadeem@14502>
2024-09-03 23:16:43 +0530

    backend completed

--- server/index.js
+++ server/index.js
@@ -16,11 +16,7 @@ const app = express();
 
 const PORT = process.env.PORT;
 mongoose
-  // .connect(
-  //   "mongodb+srv://doadmin:7I8fWg5p906Zq34O@pgdatabase-13de2ab2.mongo.ondigitalocean.com/admin"
-  // )
-  .connect("mongodb+srv://doadmin:7I8fWg5p906Zq34O@pgdatabase-13de2ab2.mongo.ondigitalocean.com/admin")
-  // .connect("mongodb://127.0.0.1:27017/PG")
+  .connect(process.env.MONGODB_URL)
   .then(() => console.log(`DataBase is connected ${PORT}`));
 
 app.use(express.json());
@@ -28,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://www.pgmanagement.tech"],
+    origin: ["https://pgmanagement-seven.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders:
       "Origin, X-Requested-With, Content-Type, Accept, Authorization",
@@ -41,10 +37,10 @@ app.use("/api/v1/pg", pgRouter);
 app.use("/api/v1/tenant", TenantsRouter);
 
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
 
+app.get("/", (req, res) => {
+  res.send("pg management backend is working...");
+});
 app.listen(PORT, () => {
   console.log("Server is working");
 });
