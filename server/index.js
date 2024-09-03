0ed8d2caa06191bfc93eba8e1fcf1e7f54b4a26a
Mohd Nadeem <105824474+codewithnadeem14502@users.noreply.github.com>
2024-09-04 00:17:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,7 +37,12 @@ app.use(
 app.options("*", cors());
 app.use("/api/v1/pg", pgRouter);
 app.use("/api/v1/tenant", TenantsRouter);
+// Define routes first
+app.get("/", (req, res) => {
+  res.send("pg management backend is working...");
+});
 
+// Serve static files
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/", (req, res) => {
